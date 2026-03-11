9e32a7cb549faf53237f796b211b81c2e2dc5522
Chindurex <chinnaracha@gmail.com>
2026-03-11 11:56:40 +0700

    add answer ex-3

--- ex-3.js
+++ ex-3.js
@@ -2,18 +2,18 @@
 
 // Start coding here
 const employee = {
-  name: "John Doe",
-  age: 18,
-  address: {
-    street: "123 Main Street",
-    city: "Cityville",
-    states: "Stateville",
-    country: "Countryland",
-    postalCode: "12345",
+                  name: "John Doe",
+                  age: 18,
+                  address: {
+                            street: "123 Main Street",
+                            city: "Cityville",
+                            states: "Stateville",
+                            country: "Countryland",
+                            postalCode: "12345",
   },
 };
 
-console.log("Employee Name: " + employees.name);
+console.log("Employee Name: " + employee.name);
 console.log("Employee Age: " + employee.age);
 console.log(
   "Employee Address: " +
@@ -25,5 +25,5 @@ console.log(
     ", " +
     employee.address.country +
     " " +
-    employee.adress.postalCode
+    employee.address.postalCode
 );
